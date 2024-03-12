3ae3e14c8418135bf2e45c6b2d0530ccd57a1f36
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-03-12 09:03:23 +0100

    implement 0n complexity

--- src/sql.py
+++ src/sql.py
@@ -2,95 +2,110 @@
 SQL Class
 """
 
+from itertools import product
+import logging
+
 
 class Sql:
 
     def __init__(self, left, right):
-        """ """
 
         self.left = left
         self.right = right
 
-        self.left_keys = sorted(set(list([i[0] for i in left])))
-        self.right_keys = sorted(set(list([i[0] for i in right])))
+    @property
+    def left_keys(self):
+        return sorted(set(list([i[0] for i in self.left])))
+
+    @property
+    def right_keys(self):
+        return sorted(set(list([i[0] for i in self.right])))
+
+    @property
+    def candidates(self):
+
+        return product(self.left, self.right)
+
+    @property
+    def inner_keys(self):
+
+        keys = product(self.left_keys, self.right_keys)
+        keys = [i[0] for i in keys if i[0] == i[1]]
+
+        logging.info(f"inner_keys is {keys}")
+
+        return sorted(list(set(keys)))
+
+    @property
+    def left_not_right(self):
+
+        keys = product(self.left_keys, self.right_keys)
+        keys = [i[0] for i in keys if i[0] not in self.right_keys]
+
+        logging.info(f"left_not_right is {keys}")
+
+        return sorted(list(set(keys)))
+
+    @property
+    def right_not_left(self):
+
+        keys = product(self.left_keys, self.right_keys)
+        keys = [i[1] for i in keys if i[1] not in self.left_keys]
+
+        logging.info(f"right_not_left is {keys}")
+
+        return sorted(list(set(keys)))
 
     def inner_join(self):
 
         final = []
-        for l in self.left:
-            for r in self.right:
 
-                # both
-                if l[0] == r[0]:
-                    ser = l + r[1:]
-                    final.append(ser)
+        for left, right in self.candidates:
+            if left[0] in self.inner_keys:
+                final.append(left + right[1:])
 
-        return sorted(final)
+        return sorted(set(list(final)))
 
     def left_join(self):
 
         final = []
-        for l in self.left:
-            for r in self.right:
 
-                # both
-                if l[0] == r[0]:
-                    ser = l + r[1:]
-                    final.append(ser)
+        for left, right in self.candidates:
 
-                # right not in left
-                elif r[0] not in self.left_keys:
-                    pass
+            if left[0] in self.inner_keys:
+                final.append(left + right[1:])
 
-                # left not in right
-                else:
-                    ser = l + (None, None)
-                    final.append(ser)
+            elif left[0] in self.left_not_right:
+                final.append(left + (None, None))
 
-        return sorted(list(set(final)))
+        return sorted(set(list(final)))
 
     def right_join(self):
 
         final = []
-        for r in self.right:
-            for l in self.left:
 
-                # both
-                if l[0] == r[0]:
-                    ser = l + r[1:]
-                    final.append(ser)
-                    continue
+        for left, right in self.candidates:
 
-                # left not in right
-                elif l[0] not in self.right_keys:
-                    continue
+            if left[0] in self.inner_keys:
+                final.append(left + right[1:])
 
-                # right not in left
-                else:
-                    ser = (r[0],) + (None, None) + r[1:]
-                    final.append(ser)
+            elif right[0] in self.right_not_left:
+                final.append((right[0],) + (None, None) + right[1:])
 
         return sorted(list(set(final)))
 
     def outter_join(self):
 
         final = []
-        for l in self.left:
-            for r in self.right:
+        for left, right in self.candidates:
 
-                # both
-                if l[0] == r[0]:
-                    ser = l + r[1:]
-                    final.append(ser)
+            if left[0] in self.inner_keys:
+                final.append(left + right[1:])
 
-                # left not in right
-                elif r[0] not in self.left_keys:
-                    ser = (r[0],) + (None, None) + r[1:]
-                    final.append(ser)
+            elif right[0] in self.right_not_left:
+                final.append((right[0],) + (None, None) + right[1:])
 
-                # right not in left
-                else:
-                    ser = l + (None, None)
-                    final.append(ser)
+            elif left[0] in self.left_not_right:
+                final.append(left + (None, None))
 
         return sorted(list(set(final)))
